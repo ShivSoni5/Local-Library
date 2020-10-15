37209531e71258c852b860506cae296ab319a7c6
Shiv Soni <shiv@neurosensum.com>
2020-10-15 11:44:38 +0530

    date format changed using moment

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -17,6 +18,10 @@ BookInstanceSchema
     .get(function () {
         return '/catalog/bookinstance/' + this._id;
     });
+
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(() => moment(this.due_back).format('MMMM Do, YYYY'));
 
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
